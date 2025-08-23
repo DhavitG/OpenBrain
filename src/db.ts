3be834d17ab014a2472f4dd551cfd2d15d5a947b
Dhavit Gandhi <gdhavit@gmail.com>
2025-08-23 18:26:23 +0530

    sign-in endpoint (v1)

---
+++ src/db.ts
@@ -0,0 +1,12 @@
+import mongoose, { model, Model, Schema } from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://dhavitg:ZRFpS2dtZt!GSDV@cluster0.rhpde.mongodb.net/OpenBrain"
+);
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+export const UserModel = model("User", UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { UserModel } from "./db.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.post("/api/v1/signup", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    await UserModel.create({
+      username: username,
+      password: password,
+    });
+
+    res.json({
+      message: "User signed up",
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exists",
+    });
+  }
+});
+
+app.post("/api/v1/signin", (req, res) => {});
+
+app.get("/api/v1/content", (req, res) => {});
+
+app.post("/api/v1/content", (req, res) => {});
+
+app.delete("/api/v1/content", (req, res) => {});
+
+app.post("/api/v1/brain/share", (req, res) => {});
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
+
+app.listen(3000);
