a70b2c54e9ae865cd7fba295d1d133c51d03b12d
Dhavit Gandhi <gdhavit@gmail.com>
2025-08-26 22:53:28 +0530

    Card component working skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,41 @@
 import Button from "./components/Button";
+import Card from "./components/Card";
 import PlusIcon from "./icons/PlusIcon";
 import ShareIcon from "./icons/ShareIcon";
 
 function App() {
   return (
-    <div className="flex gap-2">
-      <Button
-        startIcon={<PlusIcon />}
-        variant="primary"
-        text="Add content"
-        size={"md"}
-        onClick={() => {}}
-      />
-      <Button
-        startIcon={<ShareIcon />}
-        variant="secondary"
-        text="Share Brain"
-        size={"md"}
-        onClick={() => {}}
-      />
+    <div className="p-4">
+      <div className="flex justify-end gap-4">
+        <Button
+          startIcon={<PlusIcon />}
+          variant="primary"
+          text="Add content"
+          size={"md"}
+          onClick={() => {}}
+        />
+        <Button
+          startIcon={<ShareIcon />}
+          variant="secondary"
+          text="Share Brain"
+          size={"md"}
+          onClick={() => {}}
+        />
+      </div>
+
+      <div className="flex gap-4">
+        <Card
+          title="Watch"
+          type="twitter"
+          link="https://x.com/aaditsh/status/1959178840967905489?s=48"
+        />
+
+        <Card
+          title="Interesting"
+          type="youtube"
+          link="https://www.youtube.com/watch?v=KHSI0Qu4ovQ"
+        />
+      </div>
     </div>
   );
 }
